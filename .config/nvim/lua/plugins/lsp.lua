52f26c26445ce7d3aa7f34c8930dde3f757865c2
sspeedy <137386567+sspeedy@users.noreply.github.com>
2025-06-18 20:04:01 +0100

    added lsp!!!!!

---
+++ .config/nvim/lua/plugins/lsp.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      {
+        "folke/lazydev.nvim",
+        opts = {
+          library = {
+            { path = "${3rd}/luv/library", words = { "vim%.uv" } },
+          },
+        },
+      },
+    },
+    config = function()
+      vim.lsp.enable("lua_ls")
+      vim.lsp.enable("gdscript")
+    end,
+  }
+}
