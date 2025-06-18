14fa0463abfcf020673ed4cc4482a4b86dbed871
sspeedy <137386567+sspeedy@users.noreply.github.com>
2025-06-18 20:04:16 +0100

    added blink.cmp

---
+++ .config/nvim/lua/plugins/blink.cmp.lua
@@ -0,0 +1,14 @@
+return {
+  'saghen/blink.cmp',
+  -- optional: provides snippets for the snippet source
+  dependencies = { 'rafamadriz/friendly-snippets' },
+
+  version = '1.*',
+  opts = {
+    keymap = { preset = 'default' },
+    appearance = {
+      nerd_font_variant = 'mono'
+    },
+  },
+  signature = { enabled = true }
+}
