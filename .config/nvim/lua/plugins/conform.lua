5c0bcc1326e6d0c8fa3ff4795d3e9bd28c882ce3
sspeedy <137386567+sspeedy@users.noreply.github.com>
2025-06-26 20:28:58 +0100

    added conform

---
+++ .config/nvim/lua/plugins/conform.lua
@@ -0,0 +1,18 @@
+return {
+	"stevearc/conform.nvim",
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	opts = {
+		formatters_by_ft = {
+			lua = { "stylua" },
+		},
+		default_format_opts = {
+			lsp_format = "fallback",
+		},
+		format_on_save = { timeout_ms = 500 },
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+}
