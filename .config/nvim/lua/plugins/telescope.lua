9ccbc25680d15bee9e598d9b209ce6d58abff7a1
sspeedy <137386567+sspeedy@users.noreply.github.com>
2025-06-27 22:30:33 +0100

    added telescope dotfiles search

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -3,6 +3,7 @@ return {
 	dependencies = { "nvim-lua/plenary.nvim" },
 	keys = {
 		{ "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Telescope find files" },
+		{ "<leader>fd", "<cmd>Telescope find_files hidden=true<CR>", desc = "Telescope find files" },
 		{ "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Telescope live grep" },
 		{ "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Telescope buffers" },
 		{ "<leader>fh", "<cmd>Telescope help_tags<CR>", desc = "Telescope help tags" },
