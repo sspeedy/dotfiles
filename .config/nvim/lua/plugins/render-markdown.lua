56abb4b5c2fcaa138382cceab61b8eb81c6c6e31
sspeedy <137386567+sspeedy@users.noreply.github.com>
2025-07-21 00:00:47 +0100

    i stupid

---
+++ .config/nvim/lua/plugins/render-markdown.lua
@@ -0,0 +1,20 @@
+return {
+	"MeanderingProgrammer/render-markdown.nvim",
+	dependencies = {
+		"nvim-treesitter/nvim-treesitter",
+		"nvim-tree/nvim-web-devicons", -- if you prefer nvim-web-devicons
+		{
+			"3rd/image.nvim",
+			build = false, -- so that it doesn't build the rock https://github.com/3rd/image.nvim/issues/91#issuecomment-2453430239
+			opts = {
+				processor = "magick_cli",
+				integrations = {
+					markdown = {
+						only_render_image_at_cursor = true,
+					},
+				},
+			},
+		},
+	},
+	opts = {},
+}
