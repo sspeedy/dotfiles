93c7191e8e0964c3a65e73cc4cd3bf581e5036e2
speedy <137386567+sspeedy@users.noreply.github.com>
2026-05-22 21:24:54 +0100

    changed scrolling cetnering

--- .config/hypr/hyprland.lua
+++ .config/hypr/hyprland.lua
@@ -175,7 +175,7 @@ hl.config({
     scrolling = {
         fullscreen_on_one_column = true,
         column_width = 0.7,
-        focus_fit_method = 0,
+        focus_fit_method = 1,
         wrap_focus = true,
         wrap_swapcol = true,
     },
